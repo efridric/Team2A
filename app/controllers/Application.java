bdb20826b202b9f84805a8a031c969583cbbc902
efridric <efridric@uncc.edu>
2014-02-20 23:15:36 -0500

    Added twitter bootstrap, and worked on login form

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,72 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
 
+import java.util.Arrays;
+import java.util.List;
+
+import models.User;
+import play.*;
+import play.data.*;
+import static play.data.Form.*;
+import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(("hello world"));
+	public static class Login {
+
+        public String email;
+        public String password;
+        
+        public String validate() {
+        	if(User.authenticate(email, password) == null){
+        		return "Invalid username or password";
+        	}
+        	return null;
+        }
+
+	} 
+	
+	public static class SignUp {
+	    
+	    public String email;
+	    public String firstName;
+	    public String lastName;
+	    public String password;
+	}
+	
+    public static Result login() {
+        return ok(
+        	login.render(
+        	    Form.form(Login.class),
+        	    Form.form(SignUp.class)
+        	)
+        );
     }
+          
+    public static Result authenticate() {
+    	Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+    	if(loginForm.hasErrors()){
+    		return badRequest(login.render(loginForm));
+    	}
+    	else{
+    		session().clear();
+    		session("email", loginForm.get().email);
+    	}
+    	return ok();
+    }
+    
+    public static Result signUp(){
+        Form<SignUp> signUpForm = Form.form(SignUp.class).bindFromRequest();
+        if(signUpForm.hasErrors()){
+            return badRequest(login.render(signUpForm));
+        }
+        else{
+            session().clear();
+            session("email", signUpForm.get().email);
+        }
+        return ok();
+    }
+  
 
 }
